12e6e7cf69b24ab5ab56b56e53cc182985c17074
Mikhail Gurak <mikhail.gurak@gmail.com>
2022-05-30 16:28:57 +0300

    task 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,21 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+function addGalleryItemsImages(obj) {
+    const gallery = obj.map(createGalleryElement).join('');
+    galleryRef.innerHTML = gallery;
+}
+
+addGalleryItemsImages(galleryItems);
+
+function createGalleryElement({preview, original, description}) {
+    return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a>`
+};
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 20, });
